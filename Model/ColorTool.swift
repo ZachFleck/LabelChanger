497d02fe303a5ccdc6444fd75b1b8ef3e47c1a49
ZachFleck <zacfleck10@gmail.com>
2018-11-12 13:51:10 -0700

    fixed errors

---
+++ Model/ColorTool.swift
@@ -0,0 +1,26 @@
+//
+//  LabelColor.swift
+//  LabelChanger
+//
+//  Created by Fleck, Zachary on 10/11/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class ColorTool
+{
+    
+    public func createRandomColor() -> UIColor
+    {
+        let redPercent : CGFloat = CGFloat(Double(arc4random() % 256) / 255.00)
+        let bluePercent : CGFloat = CGFloat (Double(arc4random() % 256) / 255.00)
+        let greenPercent : CGFloat = CGFloat (Double(arc4random() % 256) / 255.00)
+ 
+    
+    return
+        UIColor(red:redPercent,green:greenPercent,blue:bluePercent,alpha:CGFloat(100))
+    
+    }
+}
